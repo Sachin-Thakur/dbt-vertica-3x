af32c1fbdb3ab7e74e535802dafdde5bd9bb1d26
sachinthakur96 <sachin.thakur@simplify3x.com>
2022-12-02 13:03:44 +0530

    adding sprint5

--- dbt/include/vertica/macros/materializations/models/table/create_table_as.sql
+++ dbt/include/vertica/macros/materializations/models/table/create_table_as.sql
@@ -1,14 +1,46 @@
 {% macro vertica__create_table_as(temporary, relation, sql) -%}
   {%- set sql_header = config.get('sql_header', none) -%}
 
+
   {{ sql_header if sql_header is not none }}
+  {%- set order_by = config.get('order_by', none) -%}
+ 
+ {%- set segmented_by_string = config.get('segmented_by_string', default=none) -%}
+  {%- set segmented_by_all_nodes = config.get('segmented_by_all_nodes', default=true) -%}
+  {%- set no_segmentation = config.get('no_segmentation', default=False) -%}
+  {%- set ksafe = config.get('ksafe', default=None) -%}
+
+
+
 
   create {% if temporary: -%}local temporary{%- endif %} table
     {{ relation.include(database=(not temporary), schema=(not temporary)) }}
     {% if temporary: -%}on commit preserve rows{%- endif %}
   as (
     {{ sql }}
-  );
+  )
+
+ {% if order_by is not none  %}
+ order by {{ order_by  }}
+
+{%- endif -%}
+
+
+  {% if segmented_by_string is not none -%}
+
+       segmented by {{ segmented_by_string }} {% if segmented_by_all_nodes %} ALL NODES
+
+   {% endif %}
+  {% endif %}
+
+  {% if no_segmentation %} UNSEGMENTED ALL NODES {% endif %}
+
+ {% if ksafe is not none -%}
+  ksafe {{ ksafe }}
+  {% endif %}
+
+
+  
 {% endmacro %}
 
 {% macro vertica__create_table_from_relation(temporary, relation, target, dest_columns, sql) -%}
