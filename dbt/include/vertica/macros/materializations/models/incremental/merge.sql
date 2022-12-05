1fc7e441e73492fb148cb4a5efc93fcd42e1d443
ShilpaChandrashekar15 <shilpa.chandrashekar15@gmail.com>
2022-12-05 17:55:49 +0530

    updated merge

--- dbt/include/vertica/macros/materializations/models/incremental/merge.sql
+++ dbt/include/vertica/macros/materializations/models/incremental/merge.sql
@@ -1,6 +1,7 @@
 {% macro vertica__get_merge_sql(target_relation, tmp_relation, unique_key, dest_columns) %}
   {%- set dest_columns_csv =  get_quoted_csv(dest_columns | map(attribute="name")) -%}
-  {%- set merge_columns = config.get("merge_columns", default=None)%}
+  {%- set merge_columns = config.get("unique_key", default=None)%}
+  {%- set merge_update_columns = config.get("merge_update_columns", default=dest_columns)%}
 
   merge into {{ target_relation }} as DBT_INTERNAL_DEST
   using {{ tmp_relation }} as DBT_INTERNAL_SOURCE
@@ -22,7 +23,7 @@
   {% endif %}
 
   when matched then update set
-  {% for column in dest_columns -%}
+  {% for column in merge_update_columns -%}
     {{ adapter.quote(column.name) }} = DBT_INTERNAL_SOURCE.{{ adapter.quote(column.name) }}
     {%- if not loop.last %}, {% endif %}
   {%- endfor %}
@@ -59,4 +60,37 @@
     );
 
 {%- endmacro %}
+
+-- this is working
+{% macro vertica__get_insert_overwrite_test_merge_sql(target_relation, tmp_relation, dest_columns) -%}
+    {%- set dest_cols_csv = get_quoted_csv(dest_columns | map(attribute="name")) -%}
+
+    DELETE FROM {{ target_relation }};
+
+    insert into {{ target_relation }} ({{ dest_cols_csv }})
+    (
+        select {{ dest_cols_csv }}
+        from {{ tmp_relation }}
+    );
+
+{% endmacro %}
+
+
+
+{% macro vertica__get_insert_overwrite_merge_sql(target, source, dest_columns,sql) -%}
+    {%- set partitions = config.get('partitions', default = dest_columns | map(attribute="name") | list) -%}
+    {%- set dest_cols_csv = get_quoted_csv(dest_columns | map(attribute="name")) -%}
+
+    {% for partition in partitions %}
+      SELECT DROP_PARTITIONS('{{ target_relation.schema }}.{{ target_relation.table }}', '{{ partition }}', '{{ partition }}');
+      SELECT PURGE_PARTITION('{{ target_relation.schema }}.{{ target_relation.table }}', '{{ partition }}');
+    {% endfor %}
+
+    insert into {{ target }} ({{ dest_cols_csv }})
+    (
+        select {{ dest_cols_csv }}
+        from {{ source }}
+    )
+
+{% endmacro %}
 
